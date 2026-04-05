5bca0300d8df8ba2d8dc7362135dde8454aaf75e
uie41932 <amizzuddin.amin.2.chan@aumovio.com>
2026-04-05 17:19:05 +0800

    Add python based branch

---
+++ workspace/tests/unit/test_unit.py
@@ -0,0 +1,21 @@
+################################################################################
+#  Filename:      unit/test_unit.py                                            #
+#  Project:       Web Network Visualizer                                       #
+#  Created Date:  Tuesday, May 27th 2025, 7:10:22 am                           #
+#  Author:        Amizzuddin Amin Chan                                         #
+#  Description:   Templaye unit test                                           #
+#  --------------------------------------------------------------------------- #
+#  Last Modified: Tuesday May 27th 2025 8:14:57 am                             #
+#  Modified By:   Amizzuddin Amin Chan                                         #
+#  --------------------------------------------------------------------------- #
+#  HISTORY:                                                                    #
+#  Date         By    Comments                                                 #
+#  ----------   ---   -------------------------------------------------------- #
+#  2025-05-27   AAC   Initial Implementation                                   #
+################################################################################
+
+from workspace.utilities.logger import logger
+
+
+def test_unit() -> None:
+    logger.debug("Hello Unit Test")
