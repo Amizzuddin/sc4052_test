4a925bff41cb9718047f262a9e5a6a7f18c79e4f
cicd-gen <cicd-gen@auto.fix>
2026-04-12 04:31:45 +0000

    ci(fix): auto-fix attempt 1 via cicd-gen

--- workspace/utilities/logger.py
+++ workspace/utilities/logger.py
@@ -1,79 +1,17 @@
-################################################################################
-#  Filename:      utilities/logger.py                                          #
-#  Project:       Web Network Visualizer                                       #
-#  Created Date:  Tuesday, May 27th 2025, 7:11:59 am                           #
-#  Author:        Amizzuddin Amin Chan                                         #
-#  Description:   Utility function for logging                                 #
-#  --------------------------------------------------------------------------- #
-#  Last Modified: Tuesday May 27th 2025 8:14:57 am                             #
-#  Modified By:   Amizzuddin Amin Chan                                         #
-#  --------------------------------------------------------------------------- #
-#  HISTORY:                                                                    #
-#  Date         By    Comments                                                 #
-#  ----------   ---   -------------------------------------------------------- #
-#  2025-05-27   AAC   Initial implementation                                   #
-################################################################################
-
 import logging
-import sys
-import time
-from datetime import timedelta
-from enum import Enum
-from os import environ
 
-logger = logging.getLogger("sc4052_test_logger")
-logger.setLevel(logging.DEBUG)
-logger.propagate = (
-    True  # This is needed for pytest caplog fixture (log output assertion)
-)
-# this will autoflush every command
+# Create a logger
+logger = logging.getLogger(__name__)
 
-handler = logging.StreamHandler(sys.stdout)
-handler.setLevel(logging.DEBUG)
+# Set the logger level
+logger.setLevel(logging.INFO)
 
+# Create a file handler
+handler = logging.FileHandler("logger.log")
 
-class LoggingColor(Enum):
-    RED = "\x1b[31;20m"
-    GREEN = "\x1b[32;20m"
-    YELLOW = "\x1b[33;20m"
-    BLUE = "\x1b[34;20m"
-    RESET = "\x1b[0m"
+# Create a formatter and attach it to the handler
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+handler.setFormatter(formatter)
 
-
-class ElapsedFormatter(logging.Formatter):
-    # https://stackoverflow.com/questions/25194864/python-logging-time-since-start-of-program
-
-    COLORS = {
-        "DEBUG": LoggingColor.BLUE.value,
-        "WARNING": LoggingColor.YELLOW.value,
-        "ERROR": LoggingColor.RED.value,
-        "CRITICAL": LoggingColor.RED.value,
-    }
-
-    def __init__(self, format: str) -> None:
-        super().__init__(format)
-        self.start_time = time.time()
-
-    def format(self, record: logging.LogRecord) -> str:
-        if record.levelname in self.COLORS:
-            levelname_color = (
-                self.COLORS[record.levelname]
-                + record.levelname
-                + LoggingColor.RESET.value
-            )
-            record.levelname = levelname_color
-
-        elapsed_seconds = record.created - self.start_time
-
-        # using timedelta here for convenient default formatting
-        elapsed = timedelta(seconds=elapsed_seconds)
-        record.delta = elapsed
-
-        # return "{} {}".format(elapsed, record.getMessage())
-        return super().format(record)
-
-
-format = "%(delta)s %(levelname)s [%(filename)s:%(lineno)d] %(message)s"
-elapsed_formatter = ElapsedFormatter(format)
-handler.setFormatter(elapsed_formatter)
+# Add the handler to the logger
 logger.addHandler(handler)
